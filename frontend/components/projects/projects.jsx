a89c237d11aa5f3985dd8fd2aa670b77e3e4c3b6
Anthony <Adeluca@gmx.com>
2016-11-02 21:27:35 -0400

    fix

--- frontend/components/projects/projects.jsx
+++ frontend/components/projects/projects.jsx
@@ -19,11 +19,11 @@ class Projects extends React.Component {
           </div>
           <div className="project-show right">
             <a href="https://github.com/AnthonyDeluca718/JS_Chess" className="project-link">
-              <h2>JavaScript Chess</h2>
+              <h2>React Chess</h2>
             </a>
-            <div className="project-sentence">Javascript Chess built with React/Redux. Supports both normal chess and chess with random starting board.</div>
+            <div className="project-sentence">JavaScript Chess built with React/Redux. Supports both normal chess and chess with random starting board.</div>
             <div className="project-sentence">The chess rules were written in JavaScript with a heavily object-oriented design. Each piece is an object as is the board itself. Even empty spaces are respresented by null-pieces. The frontend user interface is written in React/Redux.</div>
-            <div className="project-sentence">The object oriented design makes it easy to add features. For example the random mode was easy to set up. The only required code was a function to randomly set up the board and the react/redux code to handle the randomizer button.</div>
+            <div className="project-sentence">The object oriented design makes it easy to add features. For example the random mode was easy to set up. The only required code was a method to randomly set up the board and the react/redux code to handle the randomizer button.</div>
             <div className="project-sentence"> <a href="http://adeluca.io/JS_Chess" className="text-link">Live</a> <a href="https://github.com/AnthonyDeluca718/JS_Chess" className="text-link">Github</a></div>
           </div>
         </section>
@@ -58,7 +58,7 @@ class Projects extends React.Component {
             </a>
             <div className="project-sentence">A miniture build of JQuery that cpatures the main fucnitonality.</div>
             <div className="project-sentence">Built with vanilla Javascript and native DOM manipulations.</div>
-            <div className="project-sentence">.ajax method was used in ClonedStarter. Ajax method handles both form objects and json.</div>
+            <div className="project-sentence">Ajax method handles both form objects and json. AJAX method also automatically adds cserf tokens to the generated xhr requests. I used miniJQuery instead of full JQuery in ClonedStarter</div>
             <div className="project-sentence">Tic Tac Toe game demonstrates the effectiveness of the code.</div>
             <div className="project-sentence"> <a href="http://adeluca.io/MiniJQuery" className="text-link">Live</a> <a href="https://github.com/AnthonyDeluca718/MiniJQuery" className="text-link">Github</a></div>
           </div>
